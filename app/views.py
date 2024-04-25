a9a39487a9952a2485368b4444bbe280ccc3f072
Santiago Idarraga Ceballos <san200526@gmail.com>
2024-04-24 21:20:33 -0500

    Added view functions for each URL

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+# import requests
+import json
 
 # Create your views here.
+def init(request):
+    return render(request, 'index.html')
+def create(request):
+    return render(request, './db/create.html')
+def delete(request):
+    return render(request, './db/delete.html')
+def retrieve(request):
+    return render(request, './db/retrieve.html')
+def update(request):
+    return render(request, './db/update.html')
