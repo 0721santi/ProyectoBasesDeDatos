dc2b5b5204bb4333ee08e54af864dc635234be14
Santiago Idárraga Ceballos <san200526@gmail.com>
2024-05-22 18:29:20 -0500

    Created flask basic template

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request, render_template
 from NoSQL import NoSQL
 
-if __name__ == "__main_":
-    app = Flask(__name__, static_folder="./static", template_folder="./templates")
-    app.run(debug=True,)
-    nosql = NoSQL()
-    client = nosql.DBConn()
+app = Flask(__name__, static_folder="./static", template_folder="./templates")
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
